71dbd6e16972f8b0cdc12487de2e16e2ab51807e
BenMate <benscott98@hotmail.com>
2021-08-27 16:59:50 +1000

    added bullets, todo: vfx

---
+++ Project/Assets/Scripts/GameScripts/Bullet.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    //public GameObject hitEffect;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       // GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+       // Destroy(effect, 5.0f);
+        Destroy(gameObject);
+    }
+
+
+
+}
---
+++ Project/Assets/Scripts/GameScripts/GunShoot.cs
@@ -0,0 +1,36 @@
+
+using UnityEngine;
+
+public class GunShoot : MonoBehaviour
+{
+    public float damage = 10.0f;
+    
+    public Transform firePoint;
+    public GameObject bulletPreFab;
+
+    public float bulletForce = 20.0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }      
+    }
+
+    void Shoot()
+    {
+        //RaycastHit hit;
+        //Physics.Raycast()
+
+
+        GameObject bullet = Instantiate(bulletPreFab, firePoint.position, firePoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode.Impulse);
+
+    }
+
+}
